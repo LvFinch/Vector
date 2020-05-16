d6db542faccadb41fe35dca0f3de4c5dcd281bb2
Akko <2322264747@qq.com>
2020-05-17 03:34:23 +0800

    Update home.dart

--- vector/lib/pages/home.dart
+++ vector/lib/pages/home.dart
@@ -1,18 +1,17 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-16 23:19:43
- * -> LastEditTime : 2020-05-16 23:28:43
+ * -> LastEditTime : 2020-05-17 02:47:06
  * -> LastEditors : Akko
- * -> Description : 
+ * -> Description : Vector Home
  * -> FilePath : \vector\lib\pages\home.dart
  * -> Copyright  © 2020 Akko All rights reserved.
  */
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-  final String title;
+import './home_searchbar.dart';
 
+class HomePage extends StatefulWidget {
   @override
   _HomePage createState() => _HomePage();
 }
@@ -24,7 +23,17 @@ class _HomePage extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        leading: Icon(Icons.format_list_bulleted),
+        title: Text('Vector'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: searchBarDelegate());
+            },
+          )
+        ],
       ),
       body: Center(
         child: Column(
@@ -34,6 +43,31 @@ class _HomePage extends State<HomePage> {
               'Young',
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home), title: Text('主页')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), title: Text('设置'))
+            ],
+          ),
         ),
       ),
     );
