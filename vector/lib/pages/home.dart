72dbdcdb2a5fc57a94d81d719c31c6d55f50676f
Akko <2322264747@qq.com>
2020-05-18 00:38:22 +0800

    Update home.dart

--- vector/lib/pages/home.dart
+++ vector/lib/pages/home.dart
@@ -1,7 +1,7 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-16 23:19:43
- * -> LastEditTime : 2020-05-17 14:53:02
+ * -> LastEditTime : 2020-05-18 00:26:10
  * -> LastEditors : Akko
  * -> Description : Vector Home
  * -> FilePath : \vector\lib\pages\home.dart
@@ -9,7 +9,10 @@
  */
 import 'package:flutter/material.dart';
 
-import './home_searchbar.dart';
+// import 'package:line_icons/line_icons.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:vector/pages/home_searchbar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,31 +20,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
-//如果需要,在这里声明变量,在定义函数
+  //底部AppBar
+  FloatingActionButtonLocation _fabLocation =
+      FloatingActionButtonLocation.endDocked;
+  bool _isBottomBarNotched = true;
+  bool _isFabMini = false;
+
+  BottomAppBar _buildBottomAppBar(BuildContext context) {
+    return BottomAppBar(
+      shape: this._isBottomBarNotched ? CircularNotchedRectangle() : null,
+      color: Theme.of(context).primaryColor,
+      child: Row(
+        children: <Widget>[
+          // Bottom that pops up from the bottom of the screen.
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () => showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) => Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: Text('Dummy bottom sheet'),
+              ),
+            ),
+          ),
+          // IconButton(
+          //   icon: Icon(Icons.search),
+          //   onPressed: () =>
+          //       Fluttertoast.showToast(msg: 'Dummy search action.'),
+          // ),
+          // IconButton(
+          //   icon: const Icon(Icons.more_vert),
+          //   onPressed: () => Fluttertoast.showToast(msg: 'Dummy menu action.'),
+          // ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final drawerHeader = UserAccountsDrawerHeader(
+      accountName: Text('User Name'),
+      accountEmail: Text('user.name@email.com'),
+      currentAccountPicture: CircleAvatar(
+        child: FlutterLogo(size: 42.0),
+        backgroundColor: Colors.white,
+      ),
+      otherAccountsPictures: <Widget>[
+        CircleAvatar(
+          child: Text('A'),
+          backgroundColor: Colors.yellow,
+        ),
+        CircleAvatar(
+          child: Text('B'),
+          backgroundColor: Colors.red,
+        )
+      ],
+    );
+    final drawerItems = ListView(
+      children: <Widget>[
+        drawerHeader,
+        ListTile(
+          title: Text('To page 1'),
+          onTap: () => Navigator.of(context).push(_NewPage(1)),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('设置'),
+          onTap: () => Navigator.of(context).push(_SettingPage()),
+        ),
+        ListTile(
+          title: Text('other drawer item'),
+          onTap: () {},
+        ),
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.format_list_bulleted),
-        // leading: Builder(
-        //   builder: (BuildContext context) {
-        //     return IconButton(
-        //       icon: ClipOval(
-        //         child: Image.asset(
-        //           "img/ico/menu.png",
-        //           height: 32,
-        //           width: 35,
-        //           fit: BoxFit.cover,
-        //         ),
-        //       ),
-        //       // ;
-        //       onPressed: () {
-        //         Scaffold.of(context).openDrawer();
-        //       },
-        //     );
-        //   },
-        // ),
         title: Text('Vector'),
         centerTitle: true,
         actions: <Widget>[
@@ -53,99 +110,109 @@ class _HomePage extends State<HomePage> {
           )
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Young',
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          ),
-          boxShadow: [
-            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          ),
-          child: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home), title: Text('主页')),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.settings), title: Text('设置'))
-            ],
-          ),
-        ),
-      ),
       drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            //头部
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: UserAccountsDrawerHeader(
-                    accountName: Text("用户King"),
-                    accountEmail: Text("xxxxx.@qq.com"),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          "https://www.itying.com/images/flutter/3.png"),
-                    ),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://www.itying.com/images/flutter/2.png"),
-                          fit: BoxFit.cover),
-                    ),
-                    otherAccountsPictures: <Widget>[
-                      Image.network(
-                          "https://www.itying.com/images/flutter/4.png"),
-                      Image.network(
-                          "https://www.itying.com/images/flutter/5.png"),
-                    ],
-                  ),
-                )
+        child: drawerItems,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          ListTile(
+            title: Text('FloatingActionButton position:'),
+            trailing: DropdownButton<FloatingActionButtonLocation>(
+              value: this._fabLocation,
+              onChanged: (FloatingActionButtonLocation newVal) {
+                if (newVal != null) {
+                  setState(() => this._fabLocation = newVal);
+                }
+              },
+              items: [
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.centerFloat,
+                  child: Text('centerFloat'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.endFloat,
+                  child: Text('endFloat'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.centerDocked,
+                  child: Text('centerDocked'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.endDocked,
+                  child: Text('endDocked'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.miniStartTop,
+                  child: Text('miniStartTop'),
+                ),
+                DropdownMenuItem(
+                  value: FloatingActionButtonLocation.startTop,
+                  child: Text('startTop'),
+                ),
               ],
             ),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.home),
-              ),
-              title: Text("我的空间"),
-            ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.people),
-              ),
-              title: Text("用户中心"),
-              onTap: () {
-                Navigator.of(context).pop(); //隐藏侧边栏
-                Navigator.pushNamed(context, '/user');
+          ),
+          ListTile(
+            title: Text('Mini FAB:'),
+            trailing: Switch(
+              value: this._isFabMini,
+              onChanged: (bool val) {
+                setState(() => this._isFabMini = val);
               },
             ),
-            Divider(),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.settings),
-              ),
-              title: Text("设置中心"),
+          ),
+          ListTile(
+            title: Text('BottomAppBar notch:'),
+            trailing: Switch(
+              value: this._isBottomBarNotched,
+              onChanged: (bool val) {
+                setState(() => this._isBottomBarNotched = val);
+              },
             ),
-            Divider(),
-          ],
-        ),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        mini: this._isFabMini,
+        onPressed: () =>
+            Fluttertoast.showToast(msg: 'Dummy floating action button'),
+      ),
+      floatingActionButtonLocation: this._fabLocation,
+      bottomNavigationBar: this._buildBottomAppBar(context),
     );
   }
+}
+
+// <Null> means this route returns nothing.
+class _NewPage extends MaterialPageRoute<Null> {
+  _NewPage(int id)
+      : super(builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Page $id'),
+              elevation: 1.0,
+            ),
+            body: Center(
+              child: Text('Page $id'),
+            ),
+          );
+        });
+}
+
+class _SettingPage extends MaterialPageRoute<Null> {
+  _SettingPage()
+      : super(builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('设置'),
+              centerTitle: true,
+              elevation: 1.0,
+            ),
+            body: Center(
+              child: Text('TODO'),
+            ),
+          );
+        });
 }
