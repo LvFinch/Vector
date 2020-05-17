302b7eb0ec17294fbbb1c0f57f12094c6b36e437
Akko <2322264747@qq.com>
2020-05-17 17:51:02 +0800

    Create home_drawer.dart

---
+++ vector/lib/pages/home_drawer.dart
@@ -0,0 +1,121 @@
+// /*
+//  * -> Author : Akko
+//  * -> Date : 2020-05-17 13:42:56
+//  * -> LastEditTime : 2020-05-17 13:46:07
+//  * -> LastEditors : Akko
+//  * -> Description : 
+//  * -> FilePath : \vector\lib\pages\home_drawer.dart
+//  * -> Copyright  © 2020 Akko All rights reserved.
+//  */
+// import 'package:flutter/material.dart';
+
+// class Tabs extends StatefulWidget {
+//   final int index;
+//   Tabs({Key key, this.index = 0}) : super(key: key);
+
+//   @override
+//   _TabsState createState() => _TabsState(index);
+// }
+
+// class _TabsState extends State<Tabs> {
+//   int _currentIndex;
+//   _TabsState(index) {
+//     this._currentIndex = index;
+//   }
+//   List _pageList = [HomePage(), CategoryPage(), SettingPage()];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter Demo'),
+//       ),
+//       body: this._pageList[this._currentIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: this._currentIndex,
+//         onTap: (int index) {
+//           setState(() {
+//             this._currentIndex = index;
+//           });
+//         },
+//         // type: BottomNavigationBarType.fixed,//可以配置超过4个以上的icon
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             title: Text("首页"),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.category),
+//             title: Text("分类"),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             title: Text("设置"),
+//           ),
+//         ],
+//       ),
+//       //左侧边栏
+//       drawer: Drawer(
+//         child: Column(
+//           children: <Widget>[
+//             //头部
+//             Row(
+//               children: <Widget>[
+//                 Expanded(
+//                   child: UserAccountsDrawerHeader(
+//                     accountName: Text("用户King"),
+//                     accountEmail: Text("xxxxx.@qq.com"),
+//                     currentAccountPicture: CircleAvatar(
+//                       backgroundImage: NetworkImage(
+//                           "https://www.itying.com/images/flutter/3.png"),
+//                     ),
+//                     decoration: BoxDecoration(
+//                       image: DecorationImage(
+//                           image: NetworkImage(
+//                               "https://www.itying.com/images/flutter/2.png"),
+//                           fit: BoxFit.cover),
+//                     ),
+//                     otherAccountsPictures: <Widget>[
+//                       Image.network(
+//                           "https://www.itying.com/images/flutter/4.png"),
+//                       Image.network(
+//                           "https://www.itying.com/images/flutter/5.png"),
+//                     ],
+//                   ),
+//                 )
+//               ],
+//             ),
+//             ListTile(
+//               leading: CircleAvatar(
+//                 child: Icon(Icons.home),
+//               ),
+//               title: Text("我的空间"),
+//             ),
+//             Divider(),
+//             ListTile(
+//               leading: CircleAvatar(
+//                 child: Icon(Icons.people),
+//               ),
+//               title: Text("用户中心"),
+//               onTap: () {
+//                 Navigator.of(context).pop(); //隐藏侧边栏
+//                 Navigator.pushNamed(context, '/user');
+//               },
+//             ),
+//             Divider(),
+//             ListTile(
+//               leading: CircleAvatar(
+//                 child: Icon(Icons.settings),
+//               ),
+//               title: Text("设置中心"),
+//             ),
+//             Divider(),
+//           ],
+//         ),
+//       ),
+//       //右侧边栏
+//       endDrawer: Drawer(
+//         child: Text("右侧侧边栏"),
+//       ),
+//     );
+//   }
+// }
