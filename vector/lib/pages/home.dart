6818b9d0e6ef87869a1291fd52b6c06ec45c0cc7
Akko <2322264747@qq.com>
2020-05-17 17:51:00 +0800

    Update home.dart

--- vector/lib/pages/home.dart
+++ vector/lib/pages/home.dart
@@ -1,7 +1,7 @@
 /*
  * -> Author : Akko
  * -> Date : 2020-05-16 23:19:43
- * -> LastEditTime : 2020-05-17 02:47:06
+ * -> LastEditTime : 2020-05-17 14:53:02
  * -> LastEditors : Akko
  * -> Description : Vector Home
  * -> FilePath : \vector\lib\pages\home.dart
@@ -24,6 +24,24 @@ class _HomePage extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.format_list_bulleted),
+        // leading: Builder(
+        //   builder: (BuildContext context) {
+        //     return IconButton(
+        //       icon: ClipOval(
+        //         child: Image.asset(
+        //           "img/ico/menu.png",
+        //           height: 32,
+        //           width: 35,
+        //           fit: BoxFit.cover,
+        //         ),
+        //       ),
+        //       // ;
+        //       onPressed: () {
+        //         Scaffold.of(context).openDrawer();
+        //       },
+        //     );
+        //   },
+        // ),
         title: Text('Vector'),
         centerTitle: true,
         actions: <Widget>[
@@ -68,6 +86,64 @@ class _HomePage extends State<HomePage> {
                   icon: Icon(Icons.settings), title: Text('设置'))
             ],
           ),
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            //头部
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: UserAccountsDrawerHeader(
+                    accountName: Text("用户King"),
+                    accountEmail: Text("xxxxx.@qq.com"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://www.itying.com/images/flutter/3.png"),
+                    ),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://www.itying.com/images/flutter/2.png"),
+                          fit: BoxFit.cover),
+                    ),
+                    otherAccountsPictures: <Widget>[
+                      Image.network(
+                          "https://www.itying.com/images/flutter/4.png"),
+                      Image.network(
+                          "https://www.itying.com/images/flutter/5.png"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.home),
+              ),
+              title: Text("我的空间"),
+            ),
+            Divider(),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.people),
+              ),
+              title: Text("用户中心"),
+              onTap: () {
+                Navigator.of(context).pop(); //隐藏侧边栏
+                Navigator.pushNamed(context, '/user');
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.settings),
+              ),
+              title: Text("设置中心"),
+            ),
+            Divider(),
+          ],
         ),
       ),
     );
