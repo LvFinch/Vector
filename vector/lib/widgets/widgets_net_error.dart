a4485da6c575b9be235380fe34949355f63f4e37
Akko <2322264747@qq.com>
2020-05-18 16:21:17 +0800

    Create widgets_net_error.dart

---
+++ vector/lib/widgets/widgets_net_error.dart
@@ -0,0 +1,9 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-18 11:30:51
+ * -> LastEditTime : 2020-05-18 11:31:06
+ * -> LastEditors : Akko
+ * -> Description : 网络请求失败组件
+ * -> FilePath : \vector\lib\widgets\widgets_net_error.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
