149ee633670d46020168f67989c61023836f8c17
Akko <2322264747@qq.com>
2020-05-18 16:20:57 +0800

    Create common_text_style.dart

---
+++ vector/lib/widgets/common_text_style.dart
@@ -0,0 +1,9 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-18 11:26:04
+ * -> LastEditTime : 2020-05-18 11:26:23
+ * -> LastEditors : Akko
+ * -> Description : 一些通用的 text 样式
+ * -> FilePath : \vector\lib\widgets\common_text_style.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
