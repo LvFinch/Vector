3301955c909baaa2b022660352b38065e35b3da5
Akko <2322264747@qq.com>
2020-05-18 16:21:12 +0800

    Create widget_future_builder.dart

---
+++ vector/lib/widgets/widget_future_builder.dart
@@ -0,0 +1,9 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-18 11:27:28
+ * -> LastEditTime : 2020-05-18 11:27:45
+ * -> LastEditors : Akko
+ * -> Description : 网络请求组件
+ * -> FilePath : \vector\lib\widgets\widget_future_builder.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */ 
