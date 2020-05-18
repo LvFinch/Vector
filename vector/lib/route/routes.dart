5750394c13ff6ac23c2a012d0c3ea1e4bae02d07
Akko <2322264747@qq.com>
2020-05-18 16:20:53 +0800

    Create routes.dart

---
+++ vector/lib/route/routes.dart
@@ -0,0 +1,9 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-18 11:25:00
+ * -> LastEditTime : 2020-05-18 11:25:17
+ * -> LastEditors : Akko
+ * -> Description : fluro 的路由管理
+ * -> FilePath : \vector\lib\route\routes.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
