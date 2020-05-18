dd5e140443d4298fd3f5eb90ca33d2d55040b750
Akko <2322264747@qq.com>
2020-05-18 16:21:00 +0800

    Create h_empty_view.dart

---
+++ vector/lib/widgets/h_empty_view.dart
@@ -0,0 +1,9 @@
+/*
+ * -> Author : Akko
+ * -> Date : 2020-05-18 11:26:25
+ * -> LastEditTime : 2020-05-18 11:27:08
+ * -> LastEditors : Akko
+ * -> Description : 横向空组件（用于占位）
+ * -> FilePath : \vector\lib\widgets\h_empty_view.dart
+ * -> Copyright  © 2020 Akko All rights reserved.
+ */
